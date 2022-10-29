910cc3acc02a6657701f31dd77579be501fe94b4
seppensamadayo <108543721+seppensamadayo@users.noreply.github.com>
2022-10-29 04:09:55 +0300

    Add files via upload

---
+++ Kodlama.io.Devs/src/main/java/kodlama/io/Kodlama/io/Devs/dataAccess/abstracts/ProgrammingLanguageRepository.java
@@ -0,0 +1,15 @@
+package kodlama.io.Kodlama.io.Devs.dataAccess.abstracts;
+
+import java.util.List;
+
+import kodlama.io.Kodlama.io.Devs.entities.concretes.ProgrammingLanguage;
+
+public interface ProgrammingLanguageRepository {
+	List<ProgrammingLanguage> getAll();
+	void add(ProgrammingLanguage programmingLanguage) throws Exception;
+	void delete(ProgrammingLanguage programmingLanguage);
+	void update(ProgrammingLanguage programmingLanguage);
+	void list(ProgrammingLanguage programmingLanguage);
+	void getById(int id);
+	
+}

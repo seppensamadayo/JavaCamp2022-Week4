175cba12990eea3295518d5e14fef4e105cf8d57
seppensamadayo <108543721+seppensamadayo@users.noreply.github.com>
2022-10-29 04:24:42 +0300

    Delete Kodlama.io.Devs directory

--- Kodlama.io.Devs/src/main/java/kodlama/io/Kodlama/io/Devs/business/abstracts/ProgrammingLanguageService.java
+++
@@ -1,9 +0,0 @@
-package kodlama.io.Kodlama.io.Devs.business.abstracts;
-
-import java.util.List;
-
-import kodlama.io.Kodlama.io.Devs.entities.concretes.ProgrammingLanguage;
-
-public interface ProgrammingLanguageService {
-List<ProgrammingLanguage> getAll();
-}
